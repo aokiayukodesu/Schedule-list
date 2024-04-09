ad36cbe3896bda5369cba45196a306af86502508
aokiayukodesu <catch.you.later1031@gmail.com>
2024-04-09 02:42:30 -0700

    scheduleOneをrequestedScheduleに変更

--- src/test/java/com/example/schedule/demo/service/ScheduleServiceImplTest.java
+++ src/test/java/com/example/schedule/demo/service/ScheduleServiceImplTest.java
@@ -84,14 +84,14 @@ class ScheduleServiceImplTest {
 
     @Test
     void 指定したidが存在した場合そのidの情報を変更する() throws ScheduleNotFoundException {
-        Schedule scheduleOne = new Schedule("一泊母子旅行", LocalDate.of(2024, 04, 23), LocalTime.of(12, 00));
+        Schedule requestedSchedule = new Schedule("一泊母子旅行", LocalDate.of(2024, 04, 23), LocalTime.of(12, 00));
         doReturn(Optional.of(new Schedule(1, "予防接種", LocalDate.of(2024, 11, 15), LocalTime.of(14, 00)))).when(scheduleMapper).findById(1);
-        doNothing().when(scheduleMapper).update(1, scheduleOne);
+        doNothing().when(scheduleMapper).update(1, requestedSchedule);
 
         Schedule scheduleId = scheduleServiceImpl.findById(1);
         Schedule updateList = scheduleServiceImpl.updateList(1, new Schedule("一泊母子旅行", LocalDate.of(2024, 04, 23), LocalTime.of(12, 00)));
 
-        assertThat(updateList).isEqualTo(scheduleOne);
+        assertThat(updateList).isEqualTo(requestedSchedule);
     }
 }
 
